9592de1974dca754629e4926c981e37d623944c6
rivalcoba <ivan.rivalcoba@gmail.com>
2023-08-09 13:53:04 -0600

    🌟 Cambiar el origen de datos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
 // Importando STORE
 import { useProductStore } from './stores/ProductStore';
 // Se pone en acción el store llamado la función
-useProductStore();
+const productStore = useProductStore();
 
 </script>
 
@@ -14,7 +13,7 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
